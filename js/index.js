cc9bb1a8c432b0adbc5cbf7b94d177a6ef1ae725
manuel quinones <mfqc@gmail.com>
2018-08-04 15:44:14 -0500

    nuevo.html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-index.js
+var person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
